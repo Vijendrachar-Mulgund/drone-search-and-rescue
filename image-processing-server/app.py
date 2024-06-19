d6ee076d6ac0e714f3e353565650e75b742ab628
Vijendrachar M <44837786+Vijendrachar-Mulgund@users.noreply.github.com>
2024-06-19 02:08:39 +0100

    Basic streaming is working, test on EC2 is pending!

--- image-processing-server/app.py
+++ image-processing-server/app.py
@@ -1,9 +1,39 @@
-from flask import Flask
+from flask import Flask, Response
 from flask_socketio import SocketIO, send
+import base64
+import numpy as np
+import cv2
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SECRET!!!!!!'
 socketio = SocketIO(app, cors_allowed_origins="*")
+
+frames = []
+
+
+@socketio.on('frame')
+def handle_frame(data):
+    global frames
+    frame_data = base64.b64decode(data)
+    np_data = np.frombuffer(frame_data, dtype=np.uint8)
+    frame = cv2.imdecode(np_data, cv2.IMREAD_COLOR)
+    frames.append(frame)
+
+
+def generate_frames():
+    global frames
+    while True:
+        if frames:
+            frame = frames.pop(0)
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @socketio.on('connect')
--- raspberry-pi/main.py
+++ raspberry-pi/main.py
@@ -1,16 +1,36 @@
 from flask import Flask
 import socketio
+import cv2
+from time import time
+import base64
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SECRET!!'
 
 sio = socketio.Client()
 
+VIDEO_SOURCE = 1
+VIDEO_IMAGE_FORMAT = '.jpg'
+
+
+def send_video():
+    cap = cv2.VideoCapture(1)
+    while cap.isOpened():
+        start_time = time()
+        success, frame = cap.read()
+        if not success:
+            break
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame_data = base64.b64encode(buffer).decode('utf-8')
+        sio.emit('frame', frame_data)
+        elapsed_time = time() - start_time
+        print(f"Frame send time: {elapsed_time} seconds")
+
 
 @sio.event
 def connect():
     print('Connection established')
-    sio.send('Hello from the client!')
+    sio.start_background_task(target=send_video)
 
 
 @sio.event
