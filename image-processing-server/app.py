0f2eb8a995098d603fd05ddb7629f6b44b6c9253
Vijendrachar M <44837786+Vijendrachar-Mulgund@users.noreply.github.com>
2024-06-23 19:51:49 +0100

    Image processing server code refactored ✅

--- image-processing-server/app.py
+++ image-processing-server/app.py
@@ -2,34 +2,39 @@ import socket
 import cv2
 import numpy as np
 
+SERVER_HOST = '0.0.0.0'
+SERVER_PORT = 9999
+SERVER_ADDRESS = (SERVER_HOST, SERVER_PORT)
+SERVER_MAX_QUEUE_SIZE = 5
+IMAGE_ENCODE_DECODE_FORMAT = 'utf-8'
+VIDEO_IMAGE_ENCODE_DECODE_FORMAT = '.jpg'
 
+
+# Server Initialization
 def init_server():
     # Create a socket object
-    s_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    host = '0.0.0.0'
-    port = 9999
+    socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Bind to the port
-    s_socket.bind((host, port))
+    socket_server.bind(SERVER_ADDRESS)
     # Queue up to 5 requests
-    s_socket.listen(5)
+    socket_server.listen(SERVER_MAX_QUEUE_SIZE)
 
-    print("Server listening on {}:{}".format(host, port))
+    print("Server listening on {}:{}".format(SERVER_HOST, SERVER_PORT))
 
     # Establish a connection
-    c_socket, addr = s_socket.accept()
-    print("Got a connection from {}".format(addr))
-    return c_socket, s_socket
+    client_connection, client_address = socket_server.accept()
+    print("Got a connection from {}".format(client_address))
+    return client_connection, socket_server
 
 
-def process_data(client_conn, server_conn):
+def receive_video(client_conn, server_conn):
     while True:
         # Receive data from the client
         length = client_conn.recv(16)
         if not length:
             break
-        length = int(length.decode('utf-8'))
+        length = int(length.decode(IMAGE_ENCODE_DECODE_FORMAT))
 
         data = b''
         while len(data) < length:
@@ -41,16 +46,15 @@ def process_data(client_conn, server_conn):
         frame_data = np.frombuffer(data, dtype=np.uint8)
         frame = cv2.imdecode(frame_data, cv2.IMREAD_COLOR)
 
-        # Apply the YOLOv8 model for Object detection
         if frame is not None:
-            # Apply grayscale filter
-            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            gray_frame = cv2.cvtColor(gray_frame, cv2.COLOR_GRAY2BGR)
+            # Each frame processed here
+            processed_frame = process_frame(frame)
 
-            ret, buffer = cv2.imencode('.jpg', gray_frame)
+            # Return the frame back to the client / Record the video and store
+            ret, buffer = cv2.imencode(VIDEO_IMAGE_ENCODE_DECODE_FORMAT, processed_frame)
             if ret:
                 length = len(buffer)
-                client_conn.sendall(str(length).ljust(16).encode('utf-8'))
+                client_conn.sendall(str(length).ljust(16).encode(IMAGE_ENCODE_DECODE_FORMAT))
                 client_conn.sendall(buffer.tobytes())
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -61,12 +65,20 @@ def process_data(client_conn, server_conn):
     cv2.destroyAllWindows()
 
 
+def process_frame(image_frame):
+    # Apply filter / Run the frame through a model
+    # Apply the YOLOv8 model for Object detection
+    gray_frame = cv2.cvtColor(image_frame, cv2.COLOR_BGR2GRAY)
+    gray_frame = cv2.cvtColor(gray_frame, cv2.COLOR_GRAY2BGR)
+    return gray_frame
+
+
 if __name__ == "__main__":
     try:
         # Re-establish the server after a client disconnects
         while True:
             client_socket, server_socket = init_server()
-            process_data(client_socket, server_socket)
+            receive_video(client_socket, server_socket)
     except KeyboardInterrupt:
         print("Server shut down 🛑")
         cv2.destroyAllWindows()
